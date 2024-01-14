25b3a761f2ed40e3625a7c7eec20911a369096cb
Owen Lynch <root@owenlynch.org>
2024-01-13 16:44:41 -0800

    added build script for netlify

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+export NP_GIT=$(which git)
+wget -nv https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
+chmod +x nix-portable
+
+mkdir -p ~/.ssh
+openssl enc -d -aes-256-cbc -pbkdf2 -in netlify-nixbuild.ed25519.aes -out ~/.ssh/id_ed25519 -k "$NIXBUILD_SSH_KEY"
+chmod 600 ~/.ssh/id_ed25519
+
+echo 'eu.nixbuild.net ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM' >> ~/.ssh/known_hosts
+
+set -x
+
+./nix-portable nix build -L --eval-store auto --store ssh-ng://eu.nixbuild.net .#default
+
+
+out="$(./nix-portable nix-store --query --outputs "$(./nix-portable nix path-info --derivation .#homepage)")"
+./nix-portable nix copy --no-check-sigs --from ssh-ng://eu.nixbuild.net "$out"
+
+# The "result" symlink only valid inside the nix-portable sandbox
+./nix-portable nix run nixpkgs#coreutils -- --coreutils-prog=cp -rL "$out" output
+
+chmod -R u+w output
